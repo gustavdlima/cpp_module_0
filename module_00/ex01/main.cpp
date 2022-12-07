7e4cba38ab73bf34abd9df1faab594c59b0bc2a7
gstv <gustavdlim@gmail.com>
2022-12-07 20:45:52 -0300

    new folder organization and start ex01 from module 00

---
+++ module_00/ex01/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+// class PhoneBook
+// it has an array of contacts
+// it can store a maximum of 8 contacts. If the user tries to add 9th contact, replace the oldest one by the new one.
+// please note that dynamic allocation is forbidden.
+
+// class Contact
+// Stans for a phonebook contact
+
+// each one of classes must be instantiated as an instance.
+// anything that will always be used inside a class is private, and that anything that can be used outside
