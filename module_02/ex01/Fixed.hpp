06a84dadc4788a2a21a172f03e1a848e69fc3e4c
gstv <gustavdlim@gmail.com>
2023-01-23 17:59:41 -0300

    ex01 added [finished]

---
+++ module_02/ex01/Fixed.hpp
@@ -0,0 +1,50 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+/*
+	- the copy constructor initializes new objects
+		If a new object has to be created before the copying can occur, the copy constructor is used (note: this includes passing or returning objects by value).
+*/
+
+/*
+	- the assignment operator replaces the contents of existing objects.
+		If a new object does not have to be created before the copying can occur, the assignment operator is used.
+*/
+
+class Fixed
+{
+	public:
+		// Constructors
+		Fixed();
+		Fixed(const int num);
+		Fixed(const float num);
+		Fixed(const Fixed &copy);
+
+		// Destructor
+		~Fixed();
+
+		// Operators
+		Fixed &operator=(Fixed const& assign);
+
+		// Getters
+		int		getRawBits(void) const;
+
+		// Setters
+		void	setRawBits(const int value);
+
+		// Member Functions
+		float toFloat( void ) const;
+		int toInt( void ) const;
+
+	private:
+		int					value;
+		static const int	numberOfFractionalBits = 8;
+};
+
+std::ostream &operator<<(std::ostream& out,Fixed const &object);
+
+#endif
