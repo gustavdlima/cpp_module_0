9be4f3c45ab143c94722522ab952e1c5b55786cb
gstv <gustavdlim@gmail.com>
2023-02-10 20:14:14 -0300

    refactor: prints, comments

--- module_02/ex00/Fixed.hpp
+++ module_02/ex00/Fixed.hpp
@@ -4,16 +4,6 @@
 # include <iostream>
 # include <string>
 
-/*
-	- the copy constructor initializes new objects
-		If a new object has to be created before the copying can occur, the copy constructor is used (note: this includes passing or returning objects by value).
-*/
-
-/*
-	- the assignment operator replaces the contents of existing objects.
-		If a new object does not have to be created before the copying can occur, the assignment operator is used.
-*/
-
 class Fixed
 {
 	public:
